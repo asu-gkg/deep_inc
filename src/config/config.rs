8ebab47221c993874270f356c8751d390ea60834
未央天 <asueeer@163.com>
2024-04-01 09:41:34 +0800

    new config

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,3 +1,18 @@
+#[derive(Debug)]
+struct Config {
+    standalone: bool,
+}
+
+impl Config {
+    pub fn new(standalone: bool) -> Self {
+        Self {
+            standalone
+        }
+    }
+}
+
 pub fn say_hello() {
+    let config = Config::new(true);
+    println!("{:?}", &config);
     println!("Hello from the config mod!");
 }
