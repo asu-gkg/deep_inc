c80f1a2acb0c1d56562b2e47b8a6c62868b8b9f2
未央天 <asueeer@163.com>
2024-05-02 14:43:20 +0800

    try tch 0.16 and torch 2.3.0

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,15 @@ pub mod server;
 use crate::config::config::Config;
 use crate::server::say_hello_from_server;
 
-
+use tch::{Device, Kind, Tensor};
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
     config::config::_say_hello();
     const CALLER: &str = "Main";
     say_hello_from_server(CALLER);
-
+    let true_w = Tensor::from_slice(&[2.0, -3.4]).to_kind(Kind::Float);
+    println!("true_w: {}", true_w);
 
     let mut conf = Config::new_agg(0, 2);
     {
