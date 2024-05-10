1a0a1a60bbaa2e44209178ebe4c9cfac28e7c01d
未央天 <asueeer@163.com>
2024-05-10 13:09:43 +0800

    写了一个微测试

--- perf/run_gloo0.py
+++ perf/run_gloo0.py
@@ -3,23 +3,19 @@ import torch
 import torch.distributed as dist
 import timeit
 
-
 os.environ['MASTER_ADDR'] = '127.0.0.1'
 os.environ['MASTER_PORT'] = '29500'
 
 dist.init_process_group("gloo", rank=1, world_size=2)
 
 
-def perform_operation(dim):
-    # 生成一个随机数tensor
-    x = torch.randn(dim)
-    # 执行一个简单的操作，这里是求和
-    return dist.all_reduce(x, op=dist.ReduceOp.SUM)
+def perform_operation(dim):    x = torch.randn(dim)
 
-# 定义不同的tensor维度
-dimensions = [64, 128, 256, 512]
 
-# 测量每个维度的平均执行时间
+return dist.all_reduce(x, op=dist.ReduceOp.SUM)
+
+dimensions = [64, 128, 256]
+
 for dim in dimensions:
     time_taken = timeit.timeit('perform_operation(dim)', globals=globals(), number=100)
     print(f"Average time for dimension {dim}: {time_taken / 100:.6f} seconds")
